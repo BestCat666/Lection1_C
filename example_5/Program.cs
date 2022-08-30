b89b932b2faee27760a8bde61ea5ab816e9edf9d
Best Cat <kotova-irina11@rambler.ru>
2022-08-30 17:08:15 +0300

    Новая папка example_5

---
+++ example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "BestCat")
+{
+    Console.WriteLine("Ура, это же BestCat");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
