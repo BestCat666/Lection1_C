26800ac9d896eca63f0d2b636afee84ac3f209ab
Best Cat <kotova-irina11@rambler.ru>
2022-08-30 17:40:27 +0300

    Новая папка example_6

---
+++ example_6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int maх =a;
+
+if (a > max) maх = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("maх= ");
+Console.WriteLine(maх);
